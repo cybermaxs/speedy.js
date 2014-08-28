babd0483d707ad325fe1f07ea0c9409d201397dc
Maxime Lemaitre <maxime.lemaitre@outlook.com>
2014-08-28 14:48:23 +0200

    Build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var bump = require('gulp-bump');
 
 
 gulp.task('min', function () {
@@ -11,5 +12,12 @@ gulp.task('min', function () {
       .pipe(gulp.dest('dist/'))
 });
 
+gulp.task('bump', function () {
+    gulp.src('./*.json')
+    .pipe(bump({ type: 'patch' }))
+    .pipe(gulp.dest('./'));
+});
+
 
 gulp.task('default', ['min']);
+gulp.task('dist', ['min', 'bump']);
